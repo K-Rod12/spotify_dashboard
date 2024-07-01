b0451fb409ed998c8992341b6e20b9850757d673
Kenley Rodriguez <kenleyrodriguez12@gmail.com>
2024-06-30 20:11:26 -0400

    Fix callback

--- src/utils/script.ts
+++ src/utils/script.ts
@@ -9,7 +9,7 @@ async function redirectToAuthCodeFlow() {
   const params = new URLSearchParams();
   params.append("client_id", config.clientId);
   params.append("response_type", "code");
-  params.append("redirect_uri", "http://localhost:3000/callback");
+  params.append("redirect_uri", "https://spotify-metrics.netlify.app/callback");
   params.append("scope", "user-read-private user-read-email user-top-read playlist-modify-public user-modify-playback-state user-read-playback-state user-follow-read user-read-recently-played");
   params.append("code_challenge_method", "S256");
   params.append("code_challenge", challenge);
@@ -23,7 +23,7 @@ async function getAccessToken(clientId: string, code: string): Promise<string> {
   params.append("client_id", clientId);
   params.append("grant_type", "authorization_code");
   params.append("code", code);
-  params.append("redirect_uri", "http://localhost:3000/callback");
+  params.append("redirect_uri", "https://spotify-metrics.netlify.app//callback");
   params.append("code_verifier", verifier!);
 
   const result = await fetch("https://accounts.spotify.com/api/token", {
